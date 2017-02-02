b60b4288be1f732aa160c0234ce5707f706b7d5c
Ben Lewis <blewis@twitter.com>
2017-02-02 10:41:11 -0700

    Correct order of routes

--- server.js
+++ server.js
@@ -2,11 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+app.use(express.static('public'));
+
 app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
-
-app.use(express.static('public'));
 
 app.listen(8000, function() {
   console.log('Server listening on port 8000!');
