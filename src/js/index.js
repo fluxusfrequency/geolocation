b029fcf8b05d481b88f776622a081b582aa7f05d
Ben Lewis <blewis@twitter.com>
2017-02-02 10:59:08 -0700

    Set up geolocation callbacks

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,25 @@
-function geolocate() {
-  if (window.navigator && window.navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(() => {
-      console.log('success');
-    }, () => {
-      console.log('error');
-    });
+(() => {
+  function onSuccess(coordinates) {
+    const { latitude, longitude } = coordinates.coords;
+    console.log('coordinates', latitude, longitude);
   }
-}
 
-geolocate();
+  function onError(error) {
+    console.warn(error.code, error.message);
+    if (error.code === 1) {
+      console.log('User declined geolocation');
+    } else if (error.code === 2) {
+      console.log('Geolocation position unavailable');
+    } else if (error.code === 3) {
+      console.log('Timeout determining geolocation');
+    }
+  }
+
+  function geolocate() {
+    if (window.navigator && window.navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+    }
+  }
+
+  geolocate();
+})();
