0cecb587679c3a6116f1a18ef40d585c290efa56
Ben Lewis <blewis@twitter.com>
2017-02-02 11:29:37 -0700

    Show a pin on the map, and only make one map at a time

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,10 @@
 (() => {
-  function onSuccess(coordinates) {
+  function onGeolocateSuccess(coordinates) {
     const { latitude, longitude } = coordinates.coords;
-    console.log('coordinates', latitude, longitude);
+    showMap(latitude, longitude);
   }
 
-  function onError(error) {
+  function onGeolocateError(error) {
     console.warn(error.code, error.message);
     if (error.code === 1) {
       console.log('User declined geolocation');
@@ -15,11 +15,51 @@
     }
   }
 
+  function onLocationChange(coordinates) {
+    const { latitude, longitude } = coordinates.coords;
+    console.log('changed coordinates', latitude, longitude);
+  }
+
   function geolocate() {
     if (window.navigator && window.navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(onSuccess, onError);
+      navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
     }
   }
 
-  geolocate();
+  function watchLocation() {
+    // TODO: why can't I trigger another watch
+    if (window.navigator && window.navigator.geolocation) {
+      const watchId = navigator.geolocation.watchPosition(onLocationChange, onGeolocateError);
+      window.localStorage.setItem('lastWatch', watchId);
+    }
+  }
+
+  function clearWatch() {
+    if (window.navigator && window.navigator.geolocation) {
+      const watchId = window.localStorage.getItem('lastWatch');
+      console.log('watchId', watchId);
+      navigator.geolocation.clearWatch(watchId);
+    }
+  }
+
+  function showMap(latitude, longitude) {
+    const $map = document.getElementById('map');
+    const img = new Image();
+    img.src = 'https://maps.googleapis.com/maps/api/staticmap?center=' + latitude + ',' + longitude + '&zoom=13&size=300x300&sensor=false&markers=color:blue|' + latitude + ',' + longitude;
+
+    // Remove the old maps
+    while ($map.firstChild) {
+      $map.removeChild($map.firstChild);
+    }
+    $map.appendChild(img);
+  }
+
+  document.addEventListener('DOMContentLoaded', () => {
+    const $geolocateButton = document.getElementById('geolocation-button');
+    const $watchButton =  document.getElementById('watch-button');
+    const $clearWatchButton = document.getElementById('clear-watch-button');
+    $geolocateButton && $geolocateButton.addEventListener('click', geolocate);
+    $watchButton && $watchButton.addEventListener('click', watchLocation);
+    $clearWatchButton && $clearWatchButton.addEventListener('click', clearWatch);
+  });
 })();
