7238bc3d71f75daa456c7292ab7153c267198547
Ben Lewis <blewis@twitter.com>
2017-02-14 10:10:59 -0700

    Add nearby shops feature

--- server.js
+++ server.js
@@ -8,8 +8,9 @@ app.use('/', function(req, res) {
   res.sendFile(path.join(__dirname + '/public/index.html'));
 });
 
-app.listen(process.env.PORT || 8000, function() {
-  console.log(`Server listening on ${app.get('port')}!`);
+const port = process.env.PORT || 8000;
+app.listen(port, function() {
+  console.log(`Server listening on ${port}!`);
 });
 
 module.exports = app;
--- src/js/index.js
+++ src/js/index.js
@@ -4,6 +4,10 @@
     return;
   }
 
+  function geolocate() {
+    navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
+  }
+
   function onGeolocateSuccess(coordinates) {
     const { latitude, longitude } = coordinates.coords;
     showMap(latitude, longitude);
@@ -21,18 +25,15 @@
     }
   }
 
-  function onLocationChange(coordinates) {
-    const { latitude, longitude } = coordinates.coords;
-    console.log('Changed coordinates: ', latitude, longitude);
-  }
-
-  function geolocate() {
-    navigator.geolocation.getCurrentPosition(onGeolocateSuccess, onGeolocateError);
-  }
-
   function watchLocation() {
     const watchId = navigator.geolocation.watchPosition(onLocationChange, onGeolocateError);
     window.localStorage.setItem('lastWatch', watchId);
+    console.log('Set watchId', watchId);
+  }
+
+  function onLocationChange(coordinates) {
+    const { latitude, longitude } = coordinates.coords;
+    console.log('Changed coordinates: ', latitude, longitude);
   }
 
   function clearWatch() {
@@ -41,14 +42,15 @@
     console.log('Cleared watchId: ', watchId);
   }
 
-  function showMap(latitude, longitude) {
+  function showMap(lat, lng) {
     const $map = document.getElementById('map');
-    const position = {lat: latitude, lng: longitude};
-    const map = new google.maps.Map($map, {
+    const position = { lat, lng };
+    window.map = new google.maps.Map($map, {
       center: position,
       zoom: 6
     });
-    const marker = new google.maps.Marker({map, position});
+    window.markers = window.markers || [];
+    const marker = new google.maps.Marker({ map, position });
   }
 
   function hideMap() {
@@ -56,14 +58,57 @@
     $map.innerHTML = '';
   }
 
+  const mockShopResponse = {
+    shops: [
+      {
+        id: 1,
+        name: "The Laughing Goat",
+        lat: 40.0195,
+        lng: -105.2731
+      },
+      {
+        id: 2,
+        name: "Tee and Cakes",
+        lat: 40.0181,
+        lng: -105.2771
+      },
+      {
+        id: 3,
+        name: "The Cup",
+        lat: 40.0190,
+        lng: -105.2753
+      },
+      {
+        id: 4,
+        name: "Boxcar",
+        lat: 40.0198,
+        lng: -105.2714
+      }
+    ]
+  };
+
+  function showNearbyShops() {
+    if (!window.map || !window.markers) { return; }
+
+    mockShopResponse.shops.forEach((shop) => {
+      const { lat, lng, name } = shop;
+      const position = { lat, lng };
+      const title = name;
+      const marker = new google.maps.Marker({ map, position, title });
+      window.markers.push(marker);
+    });
+  }
+
   window.enableButtons = () => {
     const $geolocateButton = document.getElementById('geolocation-button');
     const $watchButton =  document.getElementById('watch-button');
     const $clearWatchButton = document.getElementById('clear-watch-button');
+    const $showNearbyButton = document.getElementById('show-nearby-button');
 
     $geolocateButton.disabled = false;
     $watchButton.disabled = false;
     $clearWatchButton.disabled = false;
+    $showNearbyButton.disabled = false;
 
     console.log('Google Maps API loaded');
   }
@@ -72,9 +117,11 @@
     const $geolocateButton = document.getElementById('geolocation-button');
     const $watchButton =  document.getElementById('watch-button');
     const $clearWatchButton = document.getElementById('clear-watch-button');
+    const $showNearbyButton = document.getElementById('show-nearby-button');
 
     $geolocateButton.addEventListener('click', geolocate);
     $watchButton.addEventListener('click', watchLocation);
     $clearWatchButton.addEventListener('click', clearWatch);
+    $showNearbyButton.addEventListener('click', showNearbyShops);
   });
 })();
